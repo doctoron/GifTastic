3a3fe481bd3dcfbc25123bbf858c6e3334237424
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-15 23:10:18 -0500

    Initial push

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GifTastic
 HW Assignment using the Giphy API to populate images on a page.
+My plan:
+Use  the following code  to create this app:
+The default page will use a Jamaican reggae legend. 
+Bob Marley will appear in numerous giphy images around the frame
+with a display of the current weather in Montego Bay, Jamaica.
+file:///Users/ranton/bootcamp/06-ajax/Activities/05-Bujumbura/05S-MoBay/moBay-weather.html
+
+A call to the weather will appear in the center(ish) of the page.
+
+A input field will allow the user to add their own area(s) of interest, all of which will be
+appended to the array, with a button created for each topic.  **
+**Ten images will be returned for each topic with the rating of the image displayed.
+
+Hopefully this will be deployed very soon!  
---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+var APIkey = "a4b6f03e3859400807800105c26fc9bd";
+
+// Here we are building the URL we need to query the database
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Montego%20Bay,Jamaica&units=imperial&appid=" + APIkey;
+let r;
+let s;
+//   let convertUTC;
+
+// function to convert UTC to  regular Time
+function convertUTC(x) {
+return new Date(1000 * x);
+}   
+
+// Create an AJAX call to retrieve data Log the data in console
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+      url: queryURL, 
+      method: "GET"
+  })
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+  // We put sunrise and sunset in variables for conversion from UTC to standard time
+      r = response.sys.sunrise;
+      s = response.sys.sunset;
+
+  // Log the queryURL
+    console.log(queryURL);
+    console.log(response)
+
+    // Log the resulting object
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature (Fº): " + response.main.temp);
+    console.log("Weather Description: " + response.weather[0].description);
+    console.log("Visibility: " + response.visibility);
+    console.log("Sunrise: " + convertUTC(r));
+    console.log("Sunset: " + convertUTC(s));  
+
+    
+    // Transfer content to HTML
+    $(".city").html("<h1>" + response.name + ", " + response.sys.country + " Weather Details</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".temp").text("Temperature (Fº) " + response.main.temp);
+    $(".description").text("Weather Description: " + response.weather[0].description);
+    $(".visibility").text("Visibility: " + response.visibility);
+    $(".sunrise").text("Sunrise: " + convertUTC(r));
+    $(".sunset").text("Sunset: " + convertUTC(s)); 
+
+
+});
+// Chill and say "No Problem Mon" 
