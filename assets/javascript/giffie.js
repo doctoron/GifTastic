df064501d1d0eb8df7df7c69b877fa54146f90b8
ranton <ranton@Dads-Retina-MacBookPro.local>
2019-01-18 03:53:27 -0500

    Js changes

--- assets/javascript/giffie.js
+++ assets/javascript/giffie.js
@@ -8,6 +8,8 @@ Ronald Antonio
 // Declaration of variables:
 let APIkey = "a4b6f03e3859400807800105c26fc9bd";
 let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Montego%20Bay,Jamaica&units=imperial&appid=" + APIkey;
+let queryURL2;
+let person;
 let r;
 let s;
 let topics =["Bob Marley","Jamaica","Blue Mountain","Beaches", "Coffee"];
@@ -35,13 +37,13 @@ function convertUTC(x) {
     console.log(response)
 
 // Logging the resulting object
-    console.log("Wind Speed: " + response.wind.speed);
-    console.log("Humidity: " + response.main.humidity);
-    console.log("Temperature (Fº): " + response.main.temp);
-    console.log("Weather Description: " + response.weather[0].description);
-    console.log("Visibility: " + response.visibility);
-    console.log("Sunrise: " + convertUTC(r));
-    console.log("Sunset: " + convertUTC(s));  
+    // console.log("Wind Speed: " + response.wind.speed);
+    // console.log("Humidity: " + response.main.humidity);
+    // console.log("Temperature (Fº): " + response.main.temp);
+    // console.log("Weather Description: " + response.weather[0].description);
+    // console.log("Visibility: " + response.visibility);
+    // console.log("Sunrise: " + convertUTC(r));
+    // console.log("Sunset: " + convertUTC(s));  
 
     
 // Transfer content to HTML
@@ -55,109 +57,48 @@ function convertUTC(x) {
     $(".sunset").text("Sunset: " + convertUTC(s)); 
 });
 
-function myFunction() {
-  var x = document.createElement("topics.input");
-  x.setAttribute("type", "text");
-  x.setAttribute("value", "Your turn now!");
-  document.body.appendChild(x);
-}
 
-// When button is clicked, log the queryURL + key for giphy.com API
-$("button").on("click", function() {
-    var person = $(this).attr("data-person");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    person + "&api_key=dc6zaTOxFJmzC&limit=10";
+// function myFunction() {
+//   var x = document.createElement("topics.input");
+//   x.setAttribute("type", "text");
+//   x.setAttribute("value", "Your turn now!");
+//   document.body.appendChild(x);
+// }
+
+$("#find-images").on("click", function(event) {
+  event.preventDefault();
+    console.log("Button was clicked");
+    var topics=$("#topics-input").val();
+    var person = $(this).attr("topics-input");
+    console.log(topics);
+    console.log(person);
     
-// Create an AJAX call to retrieve data Log the data in console
+    var queryURL2 = "https://api.giphy.com/v1/gifs/search?q=" +
+    topics + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
+  });
+
+// Create an AJAX call to retrieve data & log the data in console
 $.ajax({
-    url: queryURL,
-    method: "GET"
-})
-.then(function(response) {
-    var results = response.data;
-    console.log(queryURL);
-    console.log(results);
+    url: queryURL2,
+    method: "GET",
+    crossDomain: true,
+    data: JSON.stringify(topics),
+    dataType: "json"
+  })
+.done(function(response) {
+    var results2 = response.data;
+    console.log(queryURL2);
+    console.log(results2);
     
-    for (var i = 0; i < results.length; i++) {
+    for (var i = 0; i < results2.length; i++) {
           var gifDiv = $("<div>");
-          var rating = results[i].rating;
+          var rating = results2[i].rating;
           var p = $("<p>").text("Rating: " + rating);
           var personImage = $("<img>");
-          personImage.attr("src", results[i].images.fixed_height.url);
-          gifDiv.prepend(p);
-          gifDiv.prepend(personImage);
+          personImage.attr("src", results2[i].images.fixed_height.url);
+          gifDiv.append(p);
+          gifDiv.append(personImage);
           $("#gifs-appear-here").prepend(gifDiv);
         }
       });
-  });
-// Add user input to topics array
-console.log(topics);
-  $("button").on("click", function() {
-    var topics = $(this).data("topics");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-      topics + "&api_key=dc6zaTOxFJmzC&limit=10";
-    $.ajax({
-      url: queryURL,
-      method: "GET" })
-      .done(function(response) {
-        for (var i = 0; i < response.data.length; i++) {
-          var topicsDiv = $("<div>");
-          var p = $("<p>").text("Rating: " +response.data[i].rating);
-          var topicsImage = $('<img>');
-          topicsImage.attr("src",response.data[i].images.fixed_height.url);
-          topicsDiv.append(p);
-          topicsDiv.append(topicsImage);
-          $("#gifs-appear-here").append(topicsDiv);
-        }
-      })
-    })
-    // Dynamically create buttons from users input
-      // =================================================================================
-
-      // 1. Strigify the topics array
-        
-        // 2. Create a variable named "letterBtn" equal to $("<button>");
-        // var letterBtn = $("<button>");
-        // 3. Then give each "letterBtn" the following classes: "letter-button" "letter" "letter-button-color".
-        // letterBtn.addClass("letter-button letter letter-button-color");
-        // 4. Then give each "letterBtn" a data-attribute called "data-letter".
-        // letterBtn.attr("data-letter", letters[i]);
-        // 5. Then give each "letterBtns" a text equal to "letters[i]".
-        // letterBtn.text(letters[i]);
-        // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
-        // $("#buttons").append(letterBtn);
-    //   }
-
-      // Be sure to test that your code works for this major task, before proceeding to the next one!
-
-      // MAJOR TASK #2: ATTACH ON-CLICK EVENTS TO "LETTER" BUTTONS
-      // =================================================================================
-
-    // 7. Create an "on-click" event attached to the ".letter-button" class.
-        //   $(".letter-button").on("click", function() {
-        // Inside the on-click event...
-        // 8. Create a variable called "fridgeMagnet" and set the variable equal to a new div.
-        // var fridgeMagnet = $("<div>");
-        // 9. Give each "fridgeMagnet" the following classes: "letter fridge-color".
-        // fridgeMagnet.addClass("letter fridge-color");
-        // 10. Then chain the following code onto the "fridgeMagnet" variable: .text($(this).attr("data-letter"))
-        // attr acts as both a setter and a getter for attributes depending on whether we supply one argument or two
-        // NOTE: There IS a $(data) jQuery method, but it doesn't do what you'd expect. So just use attr.
-        // fridgeMagnet.text($(this).attr("data-letter"));
-        // 11. Lastly append the fridgeMagnet variable to the "#display" div (provided);
-        // Again you can see we use that find, and once its found we append the item
-        // $("#display").append(fridgeMagnet);
-        //   });
-      // Be sure to test that your code works for this major task, before proceeding to the next one!
-
-      // MAJOR TASK #3: ATTACH ON-CLICK EVENTS TO "CLEAR" BUTTON
-      // =================================================================================
-
-      // 12. Create an "on-click" event attached to the "#clear" button id.
-    //   $("#clear").on("click", function() {
-        // Inside the on-click event...
-        // 13. Use the jQuery "empty()" method to clear the contents of the "#display" div.
-        // We use find here and once its found it will empty the element
-        // $("#display").empty();
-    //   });
-    // });
